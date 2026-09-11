cca40c88130c590a41747f6d0b2c279d2d029ef1
Jonas <stivencamacho2006@gmail.com>
2026-09-10 20:49:42 -0500

    feat: Agregar formulario para registrar productos mediante POST

--- frontend-angular/src/app/components/producto-lista/producto-lista.ts
+++ frontend-angular/src/app/components/producto-lista/producto-lista.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { CommonModule, NgFor, NgIf, CurrencyPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // <--- Importar FormsModule
 import { ProductoService } from '../../services/producto';
 import { Producto } from '../../models/producto';
 
 @Component({
   selector: 'app-producto-lista',
   standalone: true,
-  imports: [CommonModule, NgFor, NgIf, CurrencyPipe],
+  imports: [CommonModule, NgFor, NgIf, CurrencyPipe, FormsModule], // <--- Agregar a imports
   templateUrl: './producto-lista.html',
   styleUrl: './producto-lista.css'
 })
 export class ProductoListaComponent implements OnInit {
   private productoService = inject(ProductoService);
-  private cdr = inject(ChangeDetectorRef); // <--- Inyectar el detector de cambios
+  private cdr = inject(ChangeDetectorRef);
   
   productos: Producto[] = [];
+
+  // Objeto para capturar los datos del formulario
+  nuevoProducto: Producto = {
+    nombre: '',
+    categoria: '',
+    precio: 0,
+    stock: 0
+  };
 
   ngOnInit(): void {
     this.cargarProductos();
@@ -23,11 +32,26 @@ export class ProductoListaComponent implements OnInit {
   cargarProductos(): void {
     this.productoService.listar().subscribe({
       next: (datos) => {
-        console.log('Datos recibidos del backend:', datos);
         this.productos = datos;
-        this.cdr.detectChanges(); // <--- Forzar a Angular a actualizar la tabla en pantalla
+        this.cdr.detectChanges();
       },
       error: (err) => console.error('Error al consultar productos:', err)
+    });
+  }
+
+  guardarProducto(): void {
+    if (!this.nuevoProducto.nombre || !this.nuevoProducto.categoria) {
+      alert('Por favor completa todos los campos.');
+      return;
+    }
+
+    this.productoService.crear(this.nuevoProducto).subscribe({
+      next: (res) => {
+        console.log('JSON Enviado / Respuesta Backend (HTTP 201 Created):', res);
+        this.cargarProductos(); // Refresca la tabla automáticamente
+        this.nuevoProducto = { nombre: '', categoria: '', precio: 0, stock: 0 }; // Limpia el formulario
+      },
+      error: (err) => console.error('Error al crear producto:', err)
     });
   }
 }
